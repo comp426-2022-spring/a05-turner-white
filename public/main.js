75c419258252ed2882b6d302ef1344cd3e0cd8c3
turner-white <turnerwhite@me.com>
2022-04-30 13:10:25 -0400

    adding function detectButton to swap nav divs

--- public/main.js
+++ public/main.js
@@ -1,14 +1,14 @@
 // Focus div based on nav button click
-function detectButton(button) {
-    document.getElementById('single').setAttribute("class", "hidden")
-    document.getElementById('home').setAttribute("class","hidden")
-    document.getElementById('multi').setAttribute("class","hidden")
-    document.getElementById('guess').setAttribute("class","hidden")
-    document.getElementById(button).setAttribute("class", "active")
+function detectButton(strName) {
+    var active = Array.from(document.getElementsByClassName("active")) // get collection of active divs
+    active.forEach(function (currentdiv) {
+        currentdiv.setAttribute("class", "hidden");
+    })
+
+    document.getElementById(strName).setAttribute("class", "active")
 }
 // Flip one coin and show coin image to match result when button clicked
 
-// Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
