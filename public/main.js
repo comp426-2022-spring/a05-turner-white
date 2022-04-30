2dbc10d7d7455e6d0d94629b9dcef6a44f1903c0
turner-white <turnerwhite@me.com>
2022-04-30 13:11:04 -0400

    adding function singleFlip to run onclick of button in html

--- public/main.js
+++ public/main.js
@@ -8,6 +8,23 @@ function detectButton(strName) {
     document.getElementById(strName).setAttribute("class", "active")
 }
 // Flip one coin and show coin image to match result when button clicked
+function singleFlip() {
+    const singleCoin = document.getElementById("single")
+    singleCoin.addEventListener("click", flipCoin)
+    function flipCoin() {
+        const url = document.baseURI+"/app/flip/"
+        fetch(url, {mode: 'cors'}).then(function(response) {
+            return response.json();
+        })
+        .then(function(something) {
+            console.log(something)
+            document.getElementById("result").innerHTML=something.flip
+            document.getElementById("quarter").setAttribute("src", "../assets/img"+something.flip+".png")
+            singleCoin.disbled = true
+        })
+    }
+}
+
 
 // Enter number and press button to activate coin flip series
 
