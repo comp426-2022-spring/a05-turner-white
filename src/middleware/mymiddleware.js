c0d5df7abb378f24a292689bb7bd445c8361dc51
turner-white <turnerwhite@me.com>
2022-04-30 13:07:58 -0400

    moved middleware functions from server.js to  middleware file

--- src/middleware/mymiddleware.js
+++ src/middleware/mymiddleware.js
@@ -1,1 +1,19 @@
 // Middleware function definitions go here
+app.use( (req, res, next) => {
+    // Your middleware goes here.
+    /* let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers["referer"],
+        useragent: req.headers["user-agent"]
+    }; */
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?,?,?,?,?,?,?,?,?,?)')
+    //const info = stmt.run(logdata.remoteaddr,logdata.remoteuser,logdata.time,logdata.method,logdata.url,logdata.protocol,logdata.httpversion,logdata.status, logdata.referer, logdata.useragent)
+    next()
+})
