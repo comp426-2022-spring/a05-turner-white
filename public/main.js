010cc72a0efeaeaf916d7a4e0d6f5b3e1061debe
turner-white <turnerwhite@me.com>
2022-04-30 13:13:33 -0400

    async functions to get results of flips and post

--- public/main.js
+++ public/main.js
@@ -88,7 +88,47 @@ function guessFlip(guess) {
 //       }
 // Flip multiple coins and show coin images in table as well as summary results
 // Our flip many coins form
+const coins = document.getElementById("coins")
+// Add event listener for coins form
+coins.addEventListener("submit", flipCoins)
+// Create the submit handler
+async function flipCoins(event) {
+    event.preventDefault();
+    
+    const endpoint = "app/flip/coins/"
+    const url = document.baseURI+endpoint
 
+    const formEvent = event.currentTarget
+
+    try {
+    const formData = new FormData(formEvent);
+    const flips = await sendFlips({ url, formData });
+
+    console.log(flips);
+    document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads;
+    document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails;
+    } catch (error) {
+    console.log(error);
+    }
+}
+// Create a data sender
+async function sendFlips({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJson = JSON.stringify(plainFormData);
+    console.log(formDataJson);
+
+    const options = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: formDataJson
+    };
+
+    const response = await fetch(url, options);
+    return response.json()
+}
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
