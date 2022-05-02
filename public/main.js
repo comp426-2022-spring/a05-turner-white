43a7aaa782edf26d53bbdb8adc033e886c508311
turner-white <turnerwhite@me.com>
2022-05-01 23:42:10 -0400

    finally

--- public/main.js
+++ public/main.js
@@ -17,9 +17,9 @@ function singleFlip() {
             return response.json();
         })
         .then(function(something) {
-            console.log(something)
+            console.log('something')
             document.getElementById("result").innerHTML=something.flip
-            document.getElementById("quarter").setAttribute("src", "../public/assets/img"+something.flip+".png")
+            document.getElementById("quarter").setAttribute("src", "../assets/img/"+something.flip+".png")
             singleCoin.disbled = true
         })
     }
